1626c30595a2b78fd112e1a36d367e3c72c7bf39
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2015-01-08 17:40:40 +0300

    heroku db related changes

---
+++ domain/src/main/java/info/riado/config/RiadoDatabaseConfiguration.java
@@ -0,0 +1,35 @@
+package info.riado.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author ivan
+ */
+@Configuration
+@Profile("production")
+public class RiadoDatabaseConfiguration {
+
+	@Bean
+	public DataSource dataSource() throws URISyntaxException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
+
+		return basicDataSource;
+	}
+
+}
