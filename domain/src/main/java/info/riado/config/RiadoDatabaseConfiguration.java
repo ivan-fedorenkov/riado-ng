412afa15a2d941fc77441ccdd0ff8ea1a259f664
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2015-01-08 21:12:31 +0300

    db script changes

--- domain/src/main/java/info/riado/config/RiadoDatabaseConfiguration.java
+++ domain/src/main/java/info/riado/config/RiadoDatabaseConfiguration.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
@@ -22,7 +22,8 @@ public class RiadoDatabaseConfiguration {
 		String password = System.getenv("DATABASE_PASSWORD");
 		String dbUrl = System.getenv("DATABASE_URL");
 
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
 		dataSource.setUrl(dbUrl);
 		dataSource.setUsername(username);
 		dataSource.setPassword(password);
